b88b8418cc1a079d2e06ddb3c83a6331f7d01899
Dominick J. Lemas <dominicklemas@gmail.com>
2019-06-27 16:38:45 -0400

    data pull operational

--- getData_emoncms.R
+++ getData_emoncms.R
@@ -36,20 +36,23 @@ api_key <-paste0("apikey=",emoncms_token)
 # https://emoncms.org/site/api#feed
 
 # start date/time
-start.time=as.POSIXct(strptime("2018-01-17 20:01:00", "%Y-%m-%d %H:%M:%S"))
-start.time.ms=as.numeric(start.time)*1000 # 1516237260000
-# Wed Jan 17 2018 20:01:00 : 1516237260000
+start.time=as.POSIXct(strptime("2018-01-01 00:00:00", "%Y-%m-%d %H:%M:%S"))
+start.time.ms=as.numeric(start.time)*1000 
+
 
 # stop date/time
-stop.time=as.POSIXct(strptime("2019-04-07 21:52:26", "%Y-%m-%d %H:%M:%S"))
-stop.time.ms=as.numeric(stop.time)*1000 # 1554688346000
-# Sun Apr 07 2019 21:52:26 : 1554688346000
+stop.time=as.POSIXct(strptime("2019-06-01 00:00:00", "%Y-%m-%d %H:%M:%S"))
+stop.time.ms=as.numeric(stop.time)*1000 
 
 # pull data
 url="https://emoncms.org/feed/data.json?";url
 full.url=paste0(url,api_key,"&id=208024&start=",start.time.ms,"&end=",stop.time.ms,"&interval=604800");full.url
+full.url=paste0(url,api_key,"&id=208024&start=",start.time.ms,"&end=",stop.time.ms,"&mode=daily");full.url
 req <- fromJSON(full.url)
 
 # format
-df1=df %>% as.data.frame() %>%
-  mutate(date=as.Date(as.POSIXct(df$V1/1000, origin="1970-01-01")))
+df1=req %>% as.data.frame() %>%
+  mutate(date=as.Date(as.POSIXct(V1/1000, origin="1970-01-01"))) %>%
+  rename(KWh=V2)
+names(df1)
+plot(df1$date,df1$KWh)
