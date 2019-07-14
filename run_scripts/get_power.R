9f391fbd7baaaca2da6c0a7dec6be852c838283f
Dominick J. Lemas <dominicklemas@gmail.com>
2019-07-14 19:48:10 -0400

    report builder works

--- run_scripts/get_power.R
+++ run_scripts/get_power.R
@@ -1,5 +1,13 @@
-library(ezknitr)
-analysis.dir=paste0(Sys.getenv("USERPROFILE"),"\\Documents\\GitHub\\emoncms\\")
-out.dir=paste0(Sys.getenv("USERPROFILE"),"\\Dropbox (UFL)\\02_Projects\\EMONCMS\\")
-ezknit(file="get_power_data.Rmd", out_dir=paste0(out.dir,"reports"))
+# load packages
+library(knitr)
+library(markdown)
+library(rmarkdown)
 
+# directories
+analysis.dir=paste0(Sys.getenv("USERPROFILE"),"\\Documents\\GitHub\\emoncms\\")
+out.dir=paste0(Sys.getenv("USERPROFILE"),"\\Dropbox (UFL)\\02_Projects\\EMONCMS\\reports\\")
+
+# execute report
+rmarkdown::render(paste0(analysis.dir,"get_power_data.Rmd"),  # file 2
+                  output_file =  paste0("report_", Sys.Date(), ".html"), 
+                  output_dir = out.dir)
